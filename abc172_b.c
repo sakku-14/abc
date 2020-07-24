8c976bf8e6963e2b3a508526e0d1ef28a05ecb75
yuki sakuma <kumasan.04.24.14@gmail.com>
2020-07-24 21:07:51 +0900

    abc172_b　マイナーチェンジの解答

---
+++ abc172_b.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define N 2*1000000
+int main(void){
+    char str[N];
+    char before[N], after[N];
+    fgets(str, sizeof(str), stdin);
+    sscanf(str, "%s", before);
+    fgets(str, sizeof(str), stdin);
+    sscanf(str, "%s", after);
+
+    int i = 0, counter = 0;
+    while(before[i] != '\0'){
+        if(strncmp(&before[i], &after[i], 1) != 0){
+            counter++;
+        }
+        i++;
+    }
+    i = 0;
+
+    printf("%d\n", counter);
+}
